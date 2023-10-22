820a65c869cb10cdaa39e5dab2bbb9d29938c93a
Lavanya1034 <122570170+Lavanya1034@users.noreply.github.com>
2023-10-22 22:12:44 +0400

    Initial setup

--- src/api/Constants.js
+++
@@ -1,5 +0,0 @@
-const Constants={
-    BASE_URL:'https://orca-app-jhg4l.ondigitalocean.app/api/',
-    IMAGE_URL:'http://rjtmobile.com/grocery/images/'
-}
-export default Constants;
--- src/redux/constants/action-types.js
+++
@@ -1,6 +0,0 @@
-export const ActionTypes = {
-    GET_NUMBER_CART:"GET_NUMBER_CART",
-    ADD_TO_CART:"ADD_TO_CART",
-    DELETE_FROM_CART:"DELETE_FROM_CART",
-    CHECK_OUT:"CHECK_OUT"
-}
